d116e3ad0a692086f829f67854f1e8b140ca8c78
Vivica <Ma>
2020-06-01 00:37:20 -0400

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react'
+  
+import React, { Component } from 'react';
 
-export default  class Cell extends Component {
-  constructor(props){
+export default class Cell extends Component {
+  
+  constructor(props) {
     super()
-
     this.state = {
       color: props.value
     }
   }
-
-  handleClick = () =>{
+  
+  handleClick = () => {
     this.setState({
       color: '#333'
     })
   }
+  
   render() {
     return (
-      <div className='cell' style={{backgroundColor:this.state.color}}
-      onClick={this.handleClick}
-      >
-        
+      <div 
+        className="cell"
+        style={{backgroundColor: this.state.color}}
+        onClick={this.handleClick}>
       </div>
     )
   }
+  
 }
-
-
--- src/Matrix.js
+++ src/Matrix.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
 import Cell from './Cell.js'
+
 export default class Matrix extends Component {
   
   genRow = (vals) => {
-   
-    return vals.map(val => <cell value={val} />)
+    return vals.map(val => <Cell value={val} />)
   }
   
   genMatrix = () => {
-    
     return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
   }
   
   render() {
-    
     return (
       <div id="matrix">
         {this.genMatrix()}
